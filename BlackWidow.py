3382df87912884a7dcf3a2d00dc674cadd8226d5
Manzini <kbmanzini81@gmail.com>
2025-01-31 13:49:06 -0500

    Updated Comments and Print Function for Welcome Branch

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,8 +1,6 @@
-# Programmer: KB Manzini
-# Branch: Welcome
-# Date:1.30.25
-#Program: BlackWidow
+print("Welcome Branch - Developer:KB Manzini")
 
-print("Welcome to InfoTechCenter v1.0")
+print("\n\tWelcome to InfoTechCenter v1.0")
+
 
 
