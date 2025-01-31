ad96696b3a283375374f9be8ad59d2fb6824da5d
Manzini <kbmanzini81@gmail.com>
2025-01-31 14:15:49 -0500

    Added Libraries & While Loop for Ellipsis - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,6 +1,23 @@
+#Libraries HERE
+import sys
+from time import sleep
+
+from google.protobuf.internal.test_bad_identifiers_pb2 import message
+
 print("Welcome Branch - Developer:KB Manzini")
 
 print("\n\tWelcome to InfoTechCenter v1.0")
 
+x = 0
+ellipsis = 0
 
-
+while x != 20:
+    x += 1
+    message = ("Infotech Center System Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
