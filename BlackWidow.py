35d79d5a26744f2ccaa88d7a3299b5d1ad6e3385
Manzini <kbmanzini81@gmail.com>
2025-02-05 14:16:09 -0500

    Added Weather Function - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,4 +1,15 @@
-# Programmer: KB Manzini
-# Branch: Weather
-# Date:
-#Program: 
+print("\n****************************\n")
+
+print("Weather Branch - Developer:KB Manzini")
+
+#Import Libraries HERE!
+import random
+from time import sleep
+
+# Weather Function to determine the weather
+def weather():
+    weatherForcastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
+    weatherCondition = random.choice(weatherForcastList)
+    return weatherCondition
+
+print(weather())
