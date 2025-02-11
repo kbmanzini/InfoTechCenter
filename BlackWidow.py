da2a661ea8d87397b8e3337b4828bf1e6778a39e
Manzini <kbmanzini81@gmail.com>
2025-02-11 13:46:44 -0500

    added VRS MPH limit and delays with sleep() (adjustable value)  - stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -22,21 +22,33 @@ def vehicleResponseSystem():
     if weatherAlert == "snowing":
         print("\nThe National Weather Service has updated our alarm by 30 minutes because"
               " it is", weatherAlert, "right now.")
+        sleep(1)
+        print("VRS has been engaged only allowing us yo drive 45MPH")
     elif weatherAlert == "blizzarding":
         print("\nThe National Weather Service has updated our alarm by 60 minutes because"
               " it is", weatherAlert, "right now.")
+        sleep(1)
+        print("VRS has been engaged only allowing you drive 30MPH")
     elif weatherAlert == "icy":
         print("\nThe National Weather Service has updated our alarm by 90 minutes because"
               " it is", weatherAlert, "right now.")
+        sleep(1)
+        print("VRS has been engaged only allowing you drive 25MPH")
     elif weatherAlert == "rainy":
         print("\nThe National Weather Service has updated our alarm by 5 minutes because"
               " it is", weatherAlert, "right now.")
+        sleep(1)
+        print("VRS has been engaged only allowing you drive 50MPH")
     elif weatherAlert == "windy":
         print("\nThe National Weather Service has updated our alarm by 5 minutes because"
               " it is", weatherAlert, "right now.")
+        sleep(1)
+        print("VRS has been engaged only allowing you drive 70MPH")
     else:
         # Default case for "sunny" or any other unexpected weather condition
         print("\nThe weather is", weatherAlert, "outside. Drive safe!")
+        sleep(1)
+        print("VRS has been disengaged drive safely!")
 
 # Call the function to execute the weather alert system
 vehicleResponseSystem()
